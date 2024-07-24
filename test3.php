5af6dcdbf1031d187a95668030be33172b4017cb
kamizu1236 <kamilakostas2003@gmail.com>
2024-07-24 10:57:43 -0400

    hola

---
+++ test3.php
@@ -0,0 +1,11 @@
+<?php
+// Definir dos números
+$num1 = 10;
+$num2 = 20;
+
+// Sumar los dos números
+$suma = $num1 + $num2;
+
+// Mostrar el resultado
+echo "La suma de $num1 y $num2 es: $suma";
+?>
