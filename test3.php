7016548553c5ed30b3c611239121ebe3b1392bd9
kamizu1236 <kamilakostas2003@gmail.com>
2024-07-24 21:20:01 -0400

    Prueba2

---
+++ test3.php
@@ -0,0 +1,13 @@
+<?php
+// Definir dos números
+$num1 = 10;
+$num2 = 20;
+
+// Sumar los dos números
+$suma = $num1 + $num2;
+
+// Mostrar el resultado
+echo "La suma de $num1 y $num2 esssssssssssssssssss: $suma";
+echo "EXMANE...";
+echo "POKER FACE..";
+?>
