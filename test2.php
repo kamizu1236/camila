b82018c682291e0222fa48097e7161287e46d3fe
kamizu1236 <kamilakostas2003@gmail.com>
2024-07-24 21:18:19 -0400

    Prueba1

---
+++ test2.php
@@ -0,0 +1,12 @@
+<?php
+// Definir dos números
+$num1 = 10;
+$num2 = 20;
+
+// Sumar los dos números
+$suma = $num1 + $num2;
+
+// Mostrar el resultado
+echo "La suma de $num1 y $num2 esssssssssssssssssss: $suma";
+echo "EXMANE...";
+?>
